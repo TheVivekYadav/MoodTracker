81e5a92de2a03d4371cd97b58ad2906f4c599fa4
thevivekyadav <vivekyadavclasher@gmail.com>
2025-03-18 00:47:09 +0530

    seperated Modal

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import {createCalendar} from "./calendar.js";
-
+import {renderMoodContainer} from "./moodContainer.js";
 
 const calendarDays = document.getElementById('calendar-days');
 
 createCalendar(calendarDays);
+
+// Initial render of the mood container
+renderMoodContainer();
 
