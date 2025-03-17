a9c6abd086e54a4dc65175bae405d627e65fa47e
thevivekyadav <vivekyadavclasher@gmail.com>
2025-03-17 20:46:07 +0530

    removed unused variable

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ import {createCalendar} from "./calendar.js";
 
 
 const calendarDays = document.getElementById('calendar-days');
-const monthYear = document.getElementById('month-year');
 
 createCalendar(calendarDays);
 
