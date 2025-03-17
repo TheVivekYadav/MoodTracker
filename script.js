ea92463615c85b7379e1d053348c7cf40d323610
thevivekyadav <vivekyadavclasher@gmail.com>
2025-03-17 20:45:33 +0530

    calendar.js is imported and used

---
+++ script.js
@@ -0,0 +1,8 @@
+import {createCalendar} from "./calendar.js";
+
+
+const calendarDays = document.getElementById('calendar-days');
+const monthYear = document.getElementById('month-year');
+
+createCalendar(calendarDays);
+
